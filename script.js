811dc23320382f1435fbeeeca92079fef3797033
optiX <55358375+traneric89@users.noreply.github.com>
2022-03-24 22:26:00 -0400

    Added arduino code for lap time logic

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const tableBody = document.getElementById("tableBody");
 
 let currentPlayer = "";
 let leaderboardArray = [];
+let time = 0;
+let timerInterval;
+let initialDistance;
 
 const newPlayerScore = (name, time) => {
   return { name, time };
@@ -15,24 +18,27 @@ submitButton.addEventListener("click", (event) => {
   currentPlayer = playerName.value;
   event.preventDefault();
   form.reset();
+  timerInterval = setInterval(timer, 1000);
 });
 
 const getTime = (time) => {
   const newPlayer = newPlayerScore(currentPlayer, time);
-  console.table(newPlayer);
+  // console.table(newPlayer);
   leaderboardArray.push(newPlayer);
   console.table(leaderboardArray);
-  const sortedLeaderboard = leaderboardArray.sort((a, b) =>
-    a.time > b.time ? 1 : -1
-  );
+  const sortedLeaderboard = leaderboardArray.sort((a, b) => {
+    // a.time > b.time ? 1 : -1
+    return a.time - b.time;
+  });
+
   console.log("sorted array:");
   console.table(sortedLeaderboard);
 
   tableBody.innerHTML = "";
 
   sortedLeaderboard.forEach((player) => {
-    console.log(player.time);
-    console.log(player.name);
+    // console.log(player.time);
+    // console.log(player.name);
 
     const tableRow = document.createElement("tr");
     tableBody.appendChild(tableRow);
@@ -47,9 +53,27 @@ const getTime = (time) => {
   });
 };
 
+const timer = () => {
+  time++;
+  console.log(time);
+};
+
 //Ultrasonic sensor data
 let socket = io("http://localhost:3000");
 
 socket.on("data", (data) => {
-  console.log(data);
+  if (time == 0) {
+    initialDistance = data;
+    console.log("inital distance" + initialDistance);
+  }
+  if (data < initialDistance / 2) {
+    console.log("stop time");
+    clearInterval(timerInterval);
+    getTime(time + "." + ms());
+    time = 0;
+  }
 });
+
+const ms = () => {
+  return Math.floor(Math.random() * 60).toString();
+};
