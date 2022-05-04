b493574412be8b86589a7cbc597b3df227856c5a
optiX <55358375+traneric89@users.noreply.github.com>
2022-05-04 12:02:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,30 +23,18 @@ submitButton.addEventListener("click", (event) => {
 
 const getTime = (time) => {
   const newPlayer = newPlayerScore(currentPlayer, time);
-  // console.table(newPlayer);
   leaderboardArray.push(newPlayer);
   console.table(leaderboardArray);
   const sortedLeaderboard = leaderboardArray.sort((a, b) => {
-    // a.time > b.time ? 1 : -1
     return a.time - b.time;
   });
-
-  console.log("sorted array:");
-  console.table(sortedLeaderboard);
-
   tableBody.innerHTML = "";
-
   sortedLeaderboard.forEach((player) => {
-    // console.log(player.time);
-    // console.log(player.name);
-
     const tableRow = document.createElement("tr");
     tableBody.appendChild(tableRow);
-
     const tableDataName = document.createElement("td");
     tableDataName.innerHTML = player.name;
     tableRow.appendChild(tableDataName);
-
     const tableDataTime = document.createElement("td");
     tableDataTime.innerHTML = `${player.time}s`;
     tableRow.appendChild(tableDataTime);
